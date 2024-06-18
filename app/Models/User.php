31cc30d045f0f307f39a1a80db29d36aabac041d
Lavanya <lavanyam@agilehealth.in>
2024-06-18 21:48:23 +0530

    feat: roles created

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Role;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -50,4 +51,18 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+    public function hasRole($roleName)
+    {
+        foreach ($this->roles as $role) {
+            if ($role->name === $roleName) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $roles = ['super_admin', 'admin','user'];
+
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+
+        $user =  User::create([
+            'first_name' => 'Tara',
+            'user_name' => 'Tara007',
+            'mobile_number' => '8973873735',
+            'email' => 'tara@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+        $role = Role::where('name', 'super_admin')->first();
+        $user->roles()->attach($role);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
